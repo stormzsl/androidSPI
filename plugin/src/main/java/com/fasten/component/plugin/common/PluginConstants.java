522ef437fdbb462c25db467e15e391b7da803198
zhangshili <zhangshili@didichuxing.com>
2019-12-10 14:43:07 +0800

    删除plugin module下无用的代码

--- plugin/src/main/java/com/fasten/component/plugin/common/PluginConstants.java
+++
@@ -1,8 +0,0 @@
-package com.fasten.component.plugin.common;
-
-/**
- * 作者:created by storm on 2019-12-07
- */
-public class PluginConstants{
-    public static final String SPI_EXTENSION_NAME="spi";
-}
